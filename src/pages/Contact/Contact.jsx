117ce2e58162b27a3ef98a442bb51c7619b7b615
Akpeti Trust <akpetitrust@gmail.com>
2022-08-13 17:43:49 +0100

    Refactored CSS

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,27 +1,43 @@
 import React, { useId } from "react";
 import { Footer, Nav } from "../../components";
-import { Logo, Mail, Curve, Ellipse } from '../../assets';
+import { Logo, Mail, Curve, Ellipse } from "../../assets";
 import style from "./index.module.css";
-const elipses = [1,2,3,4,5]
+const ellipses = [1, 2, 3, 4, 5];
 const Contact = () => {
-  const Id = useId();
+  const id = useId();
   return (
     <section className={style.contact}>
       <Nav />
       <div className={style.content}>
-        {elipses.map((a, i) => <Ellipse key={Id + i} className={style[`ellipse-${a}`]} />)}
+        {ellipses.map((a, i) => (
+          <Ellipse key={id + i} className={style[`ellipse-${a}`]} />
+        ))}
         <div className={style.illustrations}>
           <Mail />
           <Curve />
-          <Logo />
+          <Logo className={style.logo} />
         </div>
         <div className={style.details}>
-          <p>Have an idea you think will make the tool better? Or you just want to reach out? <a href="https://github.com/lego-build/lego-build/tree/contact">Send a DM on Twitter</a>, or post an <a href="https://github.com/lego-build/lego-build/tree/contact">issue on github</a>.</p>
+          <p>
+            Have an idea you think will make the tool better? Or you just want
+            to reach out? Send a{" "}
+            <a
+              href="https://github.com/lego-build/lego-build/tree/contact"
+              target="_blank"
+            >
+              DM on Twitter
+            </a>
+            , or post an{" "}
+            <a href="https://github.com/lego-build/lego-build" target="_blank">
+              issue on github
+            </a>
+            .
+          </p>
         </div>
       </div>
       <Footer />
     </section>
   );
-}
+};
 
 export default Contact;
