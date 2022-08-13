bde6086215f1d66a6f3186a330b87d4366aec368
Udoka <udochukwukaonyela@gmail.com>
2022-08-13 22:11:26 +0100

    added container

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -2,16 +2,20 @@ import React, { useId } from "react";
 import { Footer, Nav } from "../../components";
 import { Logo, Mail, Curve, Ellipse } from "../../assets";
 import style from "./index.module.css";
-const ellipses = [1, 2, 3, 4, 5];
+const ellipses = [1, 2, 3, 4, 5,6];
 const Contact = () => {
   const id = useId();
   return (
     <section className={style.contact}>
       <Nav />
       <div className={style.content}>
+        <div className={style.blobs}>
+        <div className={style.blobContainer}>
         {ellipses.map((a, i) => (
           <Ellipse key={id + i} className={style[`ellipse-${a}`]} />
         ))}
+        </div>
+        </div>
         <div className={style.illustrations}>
           <Mail />
           <Curve />
