cbe14687b720d10021bcb1b84bc2cce1cb2fa33c
Udoka <udochukwukaonyela@gmail.com>
2022-08-16 00:05:40 +0100

    styling

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import style from "./index.module.css";
-
+const Documentation = ["Installation", "Quick Start", "Philosophy", "Commands", "Options", "Summary"];
 function SideNav() {
   return (
     <aside className={style.nav}>
-      <nav>Sticky</nav>
+      <nav>
+        <div className={style.containerDoc}>
+          <h3 className={style.header}>Documentation</h3>
+          <a href="#installation">Installation</a>
+          <a href="#quickstart">Quick Start</a>
+          <a href="#philosophy">Philosophy</a>
+          <a href="#commands">Commands</a>
+          <a href="#options">Options</a>
+          <a href="#summary">Summary</a>
+        </div>
+        <div className={style.containerRef}>
+          <h3 className={style.header}>REFERENCES</h3>
+          <a href="#blocks">Blocks</a>
+          <a href="#file">File Formats</a>
+        </div>
+      </nav>
     </aside>
   );
 }
