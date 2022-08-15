38bde88b31bbd523c14889ccd83fcb78f2f0b8a0
Udoka <udochukwukaonyela@gmail.com>
2022-08-16 00:35:06 +0100

    created side bar

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./index.module.css";
 const Documentation = ["Installation", "Quick Start", "Philosophy", "Commands", "Options", "Summary"];
 function SideNav() {
+  const [drop, setDrop] = useState(false)
   return (
     <aside className={style.nav}>
       <nav>
@@ -9,7 +10,18 @@ function SideNav() {
           <h3 className={style.header}>Documentation</h3>
           <a href="#installation">Installation</a>
           <a href="#quickstart">Quick Start</a>
-          <a href="#philosophy">Philosophy</a>
+          <span>
+            <span className={style.philosophy} onClick={()=> setDrop((prev)=>!prev)}>Philosophy</span>
+            {drop &&
+              <span className={style.containerPhilosophy}>
+            <a href="#blocks">Blocks</a>
+            <a href="#file-formats">File Formats</a>
+            <a href="#templates">Templates</a>
+            <a href="#json-structure">JSON Structure</a>
+              </span>
+            }
+          </span>
+            
           <a href="#commands">Commands</a>
           <a href="#options">Options</a>
           <a href="#summary">Summary</a>
