75f57147d64d053ee6908a3d030684eb4eb549f3
Udoka <udochukwukaonyela@gmail.com>
2022-08-17 22:15:10 +0100

    creating scroll toggle

--- src/pages/Docs/Docs.jsx
+++ src/pages/Docs/Docs.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Footer, Nav, SideNav } from "../../components";
 import style from "./index.module.css";
@@ -10,6 +10,32 @@ function Docs() {
   let component;
 
   if (!subpage) component = <Main />;
+useEffect(() => {
+  const link = document.querySelectorAll('a#link')
+  ,sections = document.querySelectorAll('section')
+  console.log(sections);
+  const toggleLogic = () => {
+    let current = '';
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      /* eslint-disable */
+      return (pageYOffset >= (sectionTop - sectionHeight / 3)) ?
+        current = section.getAttribute('id')
+        :
+        current;
+
+    });
+    link.forEach(li => {
+      li.classList.remove('active');
+      if (li.classList.contains(current)) {
+        li.classList.add('active');
+      }
+    })
+  }
+  window.addEventListener('scroll', toggleLogic);
+  return window.removeEventListener('scroll', toggleLogic)
+}, [])
 
   return (
     <div className={style.docs}>
