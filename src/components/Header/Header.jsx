06a7da3fe308bdd0d4693c00dd903500b2cc067e
Udoka <udochukwukaonyela@gmail.com>
2022-08-21 16:09:40 +0100

    created header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Search from "../Search/Search"
 
 const Header = () => {
   return (
       <header>
           <div><h1>Browse Project Workflows</h1></div>
           <p>Check out different workflows and templates to fit your project’s needs. Try searching for ‘TypeScript’, or ‘hooks’.</p>
+          <Search />
     </header>
   )
 }
