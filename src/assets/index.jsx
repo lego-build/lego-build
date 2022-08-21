756553aac1aa414411629dd264ea204638b6ee0b
Udoka <udochukwukaonyela@gmail.com>
2022-08-21 14:31:50 +0100

    exported assets

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -2,4 +2,5 @@ export { default as Logo } from "./illustrations/Logo";
 export { default as Curve } from "./illustrations/Curve";
 export { default as Mail } from "./illustrations/Mail";
 export { default as Ellipse } from "./illustrations/Ellipse";
+export { default as Search } from "./icons/SearchBar";
 
