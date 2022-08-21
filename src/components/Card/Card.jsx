ecd000f88fcd7b7d1086f6b46ba330de5337f659
Udoka <udochukwukaonyela@gmail.com>
2022-08-21 20:51:26 +0100

    added meta data

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Preview from "../Preview/Preview";
+import { wordParser } from "../../utils/functions";
 import style from "./index.module.css";
-const Card = ({ id, title, description, name, profileLink }) => {
+const Card = ({ data: { id, title, description } }) => {
+  console.log({ id, title, description})
   return (
-    <div className={style.card}>
-      <Preview link={profileLink} />
+    <div className={style.card} data-mark={`${wordParser(!!title ? title : "")}`}>
+mmm
     </div>
   )
 }
