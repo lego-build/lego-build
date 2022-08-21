e4f32c5cc5055f08fa7e075351c7536526e5dd37
Udoka <udochukwukaonyela@gmail.com>
2022-08-21 16:59:46 +0100

    styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import style from "./index.module.css";
 const Header = () => {
   return (
-      <header>
+    <header className={style.header}>
           <div><h1>Browse Project Workflows</h1></div>
           <p>Check out different workflows and templates to fit your project’s needs. Try searching for ‘TypeScript’, or ‘hooks’.</p>
     </header>
