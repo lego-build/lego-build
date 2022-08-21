540d35b1ce4275473693a3f2967255746d59c7d8
Udoka <udochukwukaonyela@gmail.com>
2022-08-21 17:09:21 +0100

    exported componeny

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -4,4 +4,7 @@ export { default as Mail } from "./illustrations/Mail";
 export { default as Ellipse } from "./illustrations/Ellipse";
 export { default as SearchBar } from "./icons/SearchBar";
 export { default as Clear } from "./icons/Clear";
+export { default as First } from "./icons/First";
+export { default as Second } from "./icons/Second";
+export { default as Third } from "./icons/Third";
 
