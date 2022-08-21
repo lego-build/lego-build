9a0187f154313cdc8194d8679ad962ccfacf991f
Udoka <udochukwukaonyela@gmail.com>
2022-08-21 14:24:46 +0100

    created header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+      <header>
+          <div><h1>Browse Project Workflows</h1></div>
+          <p>Check out different workflows and templates to fit your project’s needs. Try searching for ‘TypeScript’, or ‘hooks’.</p>
+    </header>
+  )
+}
+
+export default Header
