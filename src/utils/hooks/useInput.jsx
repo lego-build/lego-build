f54b43ec92ce60daca967f379eb298cfdff9fa7d
Udoka <udochukwukaonyela@gmail.com>
2022-08-21 16:20:23 +0100

    created useInput hook

---
+++ src/utils/hooks/useInput.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+const useInput = initialValue => {
+    const [value, setValue] = useState(initialValue);
+    return [
+        { value, onChange: e => setValue(e.target.value) },
+        () => setValue(initialValue)
+    ];
+}
+
+export default useInput
