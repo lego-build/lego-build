15db924dc87bca85933e399d2712fe517d4de3d3
Udoka <udochukwukaonyela@gmail.com>
2022-08-21 20:52:25 +0100

    added keys and fixed mapping

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Card from "../Card/Card"
 import style from "./index.module.css"
-const Cards = ({data}) => {
+import { useId } from 'react';
+const Cards = ({ data }) => {
+    const Id = useId();
   return (
       <div className={style.container}>
           <a href="/#">Contribute Yours</a>
-      <div className={style.cards}>
-          {
-              data.map((details)=>(<Card data={details} />))
-          }
-      </div>
+        <div className={style.cards}>
+            {data.map((details, i) => (<Card data={details} key={Id + i} />))}
+        </div>
       </div>
   )
 }
