c5f4c3efae0c7cf55b021d9be10f754bc237d6a7
Udoka <udochukwukaonyela@gmail.com>
2022-08-21 16:10:06 +0100

    search component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import { useId } from 'react';
+import { SearchBar } from "../../assets";
+import style from "./index.module.css";
 const Search = () => {
+    const Id = useId();
   return (
-      <div>
-          
+      <div className={style.search}>
+          <label for={Id} className={style.label}>
+              <span className={style.icon}><SearchBar/> </span>
+              <input type={"text"} id={Id} className={style.input} placeholder="Search for workflow" />
+          </label>
       </div>
   )
 }
