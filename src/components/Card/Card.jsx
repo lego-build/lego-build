ce0adb9fbe3b6a675d175047daf371a7067be84b
Udoka <udochukwukaonyela@gmail.com>
2022-08-26 07:17:25 +0100

    added parsers

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,7 +4,8 @@ import style from "./index.module.css";
 import { Link } from "react-router-dom";
 const Card = ({ id, title, description, author: { name, profileLink } } ) => {
   return (
-    <Link to={parser(title)} className={style.card}>
+    <Link to={parser(title)}>
+    <div className={style.card}>
       <div className={style.header} data-mark={wordParser(!!title ? title : "")}>
         <p>{wordParser(!!title ? title : "")}</p>
       </div>
@@ -12,6 +13,7 @@ const Card = ({ id, title, description, author: { name, profileLink } } ) => {
       <h1 className={style.title}>{title}</h1>
       <p>{description}</p>
         <span className={style.link}>By <a href={profileLink}>{name}</a></span>
+      </div>
       </div>
     </Link>
   )
