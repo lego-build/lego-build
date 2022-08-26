9e7bdabe03846ebf003cb578574dd282f3da19a1
Udoka <udochukwukaonyela@gmail.com>
2022-08-26 11:06:22 +0100

    fixed search logic

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { useId } from 'react';
 import { SearchBar, Clear } from "../../assets";
 import style from "./index.module.css";
-const Search = ({ value: {value, onChange}, resetValue }) => {
+const Search = ({ value, resetValue }) => {
     const Id = useId();
   return (
       <div className={style.search}>
           <label htmlFor={Id} className={style.label}>
               <span className={style.icon}><SearchBar/> </span>
-              <input value={value} onChange={onChange} type={"text"} id={Id} className={style.input} placeholder="Search for workflow" />
-              <span className={`${style.clear} ${value === "" ? style.hidden : style.show}`} onClick={resetValue}><Clear /> </span>
+              <input value={value} onChange={(e)=>resetValue(e.target.value)} type={"text"} id={Id} className={style.input} placeholder="Search for workflow" />
+              <span className={`${style.clear} ${value === "" ? style.hidden : style.show}`} onClick={()=>resetValue("")}><Clear /> </span>
           </label>
           <a href="/#" className={style.contribute}>Contribute Yours</a>
       </div>
