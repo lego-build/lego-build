aa2451b96d9860b1e33c9a7084459ed2828c6f94
Udoka <udochukwukaonyela@gmail.com>
2022-08-26 13:48:51 +0100

    created setup

--- src/pages/Community/subpage/Setup/Setup.jsx
+++ src/pages/Community/subpage/Setup/Setup.jsx
@@ -3,8 +3,14 @@ import style from "./index.module.css";
 const Setup = ({ author:{name,profileLink }, description, id, title }) => {
     return (
         <div className={style.setup}>
-            {/* {name}{profileLink}{description}{id}{title} */}
-            <span className={style.link}>By <a href={profileLink}>{name}</a></span>
+            <div className={style.content}>
+                <span className={style.link}>By <a href={profileLink}>{name}</a></span>
+                <h1>{title}</h1>
+
+            </div>
+            <div className={style.terminal}>
+
+            </div>
         </div>
     );
 }
