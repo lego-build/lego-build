a4cdeac002a48e8be39cd55f49115369d43780b3
Udoka <udochukwukaonyela@gmail.com>
2022-08-26 11:06:35 +0100

    fixed up community page

--- src/pages/Community/Community.jsx
+++ src/pages/Community/Community.jsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Cards, Header, Layout } from "../../components";
-import useInput from '../../utils/hooks/useInput';
+import { Layout } from "../../components";
 import style from "./index.module.css";
-import { Search } from "../../components";
 import Main from "./subpage/Main/Main";
 import { useParams } from "react-router-dom";
 import { parserUndo } from "../../utils/functions";
 import Setup from "./subpage/Setup/Setup";
 function Community() {
-  const [value, resetValue] = useInput("");
+  const [value, resetValue] = useState("");
   const { setup } = useParams();
 
   const [workflows, setWorkflows] = useState([
@@ -48,6 +46,43 @@ function Community() {
     const [value] = workflows.filter(({ title }) => title === parserUndo(setup))
     component = <Setup {...value} />
   }
+  useEffect(() => {
+    const initialValue = [
+      {
+        id: 1,
+        title: "TypeScript Setup",
+        description:
+          "Workflow for React + TypeScript, including components, pages, hooks and redux blocks. Use it to speed up your workflow.",
+        author: {
+          name: "Akpeti Trust",
+          profileLink: "https://github.com/AkpetiTrust",
+        },
+      },
+      {
+        id: 2,
+        title: "React Hooks",
+        description:
+          "Workflow for React + TypeScript, including components, pages, hooks and redux blocks. Use it to speed up your workflow.",
+        author: {
+          name: "Akpeti Trust",
+          profileLink: "https://github.com/AkpetiTrust",
+        },
+      },
+      {
+        id: 3,
+        title: "Vue Workflow",
+        description:
+          "Workflow for Vue, including components, pages, hooks and redux blocks. Use it to speed up your workflow.",
+        author: {
+          name: "Udoka",
+          profileLink: "https://github.com/Onyelaudochukwuka",
+        },
+      },
+    ]
+    const filteredData = initialValue.filter(({ title }) => title.toLowerCase().includes(value.toLowerCase()));
+    setWorkflows(filteredData);
+    console.log(value)
+  }, [value])
   return (
     < Layout className={style.community} >
     { component }
