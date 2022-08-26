558e81f3e23e133b6c52e1fae12510b6ef51981f
Udoka <udochukwukaonyela@gmail.com>
2022-08-26 07:17:12 +0100

    fixes

--- src/assets/icons/Clear.jsx
+++ src/assets/icons/Clear.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Clear = (props) => {
   return (
-        <svg {...props} width="15px" height="15px" viewBox="0 0 24 24" id="a2495f07-7746-4c27-a7cc-72112ecafc37" dataName="Livello 1" xmlns="http://www.w3.org/2000/svg"><title>prime</title><polygon id="c8ef3de6-eeb3-441f-adf3-4bc928b83c76" data-name="clear" points="18.41 2.36 12.05 8.73 5.69 2.36 2.86 5.19 9.22 11.56 2.86 17.92 5.69 20.75 12.05 14.38 18.41 20.75 21.24 17.92 14.88 11.56 21.24 5.19 18.41 2.36" /></svg>
+        <svg {...props} width="15px" height="15px" viewBox="0 0 24 24" id="a2495f07-7746-4c27-a7cc-72112ecafc37" xmlns="http://www.w3.org/2000/svg"><title>prime</title><polygon id="c8ef3de6-eeb3-441f-adf3-4bc928b83c76" data-name="clear" points="18.41 2.36 12.05 8.73 5.69 2.36 2.86 5.19 9.22 11.56 2.86 17.92 5.69 20.75 12.05 14.38 18.41 20.75 21.24 17.92 14.88 11.56 21.24 5.19 18.41 2.36" /></svg>
   )
 }
 
