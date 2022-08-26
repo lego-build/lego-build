42e2d07fa577410ca34e1d50c7cae550d65624a5
Udoka <udochukwukaonyela@gmail.com>
2022-08-26 12:27:14 +0100

    added highlights

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import { First, Second, Third } from "../../assets";
 import style from "./index.module.css";
 const Header = () => {
   return (
     <header className={style.header}>
-      
-          <div><h1>Browse Project Workflows</h1> </div>
+      <div><h1>Browse Project Workflows</h1><span><First className={style.First} /><Second className={style.Second} /><Third className={style.Third} /></span></div>
           <p>Check out different workflows and templates to fit your project’s needs. Try searching for ‘TypeScript’, or ‘hooks’.</p>
     </header>
   )
