5b45482f83f45550023cbca7bf63b3cde5354401
Udoka <udochukwukaonyela@gmail.com>
2022-08-26 13:42:53 +0100

    added top link

--- src/pages/Community/subpage/Setup/Setup.jsx
+++ src/pages/Community/subpage/Setup/Setup.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import style from "./index.module.css";
 const Setup = ({ author:{name,profileLink }, description, id, title }) => {
     return (
-        <div>
-            {name}{profileLink}{description}{id}{title}
+        <div className={style.setup}>
+            {/* {name}{profileLink}{description}{id}{title} */}
+            <span className={style.link}>By <a href={profileLink}>{name}</a></span>
         </div>
     );
 }
