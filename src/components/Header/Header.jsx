88d2f90eb2a7f50c5ddefde159d32adca2efc3c2
Udoka <udochukwukaonyela@gmail.com>
2022-08-26 11:40:14 +0100

    fixes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Crystals } from "../../assets";
 import style from "./index.module.css";
 const Header = () => {
   return (
     <header className={style.header}>
+      <span className={style.Crystals}>
+        <Crystals className={style.Crystal1} />
+        <Crystals className={style.Crystal2} />
+      </span>
           <div><h1>Browse Project Workflows</h1> </div>
           <p>Check out different workflows and templates to fit your project’s needs. Try searching for ‘TypeScript’, or ‘hooks’.</p>
     </header>
