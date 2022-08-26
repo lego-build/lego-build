45c0af6e137a7fd7830408d1bf9477fa8e6b721e
Udoka <udochukwukaonyela@gmail.com>
2022-08-26 12:26:49 +0100

    added props

--- src/assets/icons/Third.jsx
+++ src/assets/icons/Third.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Third = () => {
+const Third = (props) => {
   return (
-      <svg width="14" height="4" viewBox="0 0 14 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg {...props} width="14" height="4" viewBox="0 0 14 4" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fillRule="evenodd" clipRule="evenodd" d="M12.0823 0.743238C8.76136 0.908846 5.44037 1.07413 2.11883 1.23974C1.35814 1.23974 0.770787 1.90249 0.807773 2.67533C0.84531 3.44816 1.49283 3.99987 2.25408 3.99987C5.58059 3.83426 8.90654 3.66897 12.2325 3.50337C12.9932 3.44816 13.5767 2.78563 13.5353 2.01279C13.4939 1.29516 12.8425 0.688035 12.0823 0.743238Z" fill="#008F75" />
       </svg>
   )
