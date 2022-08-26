c0d6e06aa47be40fdc42678e0bb2dc39999916e2
Udoka <udochukwukaonyela@gmail.com>
2022-08-26 12:26:43 +0100

    added props

--- src/assets/icons/First.jsx
+++ src/assets/icons/First.jsx
@@ -1,9 +1,9 @@
 
 import React from 'react'
 
-const First = () => {
+const First = (props) => {
   return (
-      <svg width="5" height="14" viewBox="0 0 5 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg {...props} width="5" height="14" viewBox="0 0 5 14" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fillRule="evenodd" clipRule="evenodd" d="M2.7083 12.3797C3.69974 8.84672 4.19987 5.14783 4.65805 1.55966C4.75411 0.786824 4.21809 0.0692983 3.46292 0.0140958C2.7072 -0.0963094 2.01606 0.45604 1.92001 1.17367C1.47894 4.65144 1.00696 8.23928 0.0525104 11.6066C-0.154499 12.3243 0.271659 13.0974 1.00475 13.3182C1.73729 13.5391 2.50074 13.0973 2.7083 12.3797Z" fill="#008F75" />
       </svg>
   )
