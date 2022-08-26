5437e3a9d11c80f4e03ecab6ae3dcdf413fe837e
Udoka <udochukwukaonyela@gmail.com>
2022-08-26 11:06:41 +0100

    fixes

--- src/pages/Community/subpage/Setup/Setup.jsx
+++ src/pages/Community/subpage/Setup/Setup.jsx
@@ -1,8 +1,10 @@
-const Setup = (props) => {
-    console.log(props)
+import React from "react";
+import style from "./index.module.css";
+const Setup = ({ author:{name,profileLink }, description, id, title }) => {
     return (
-        <>
-        </>
+        <div>
+            {name}{profileLink}{description}{id}{title}
+        </div>
     );
 }
  
