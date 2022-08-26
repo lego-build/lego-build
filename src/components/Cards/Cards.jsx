aca1ea808396bc304de9ec0afded0fd0f535e770
Udoka <udochukwukaonyela@gmail.com>
2022-08-26 11:06:11 +0100

    created cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Card from "../Card/Card"
 import style from "./index.module.css"
 import { useId } from 'react';
+import { Dots } from "../../assets";
 const Cards = ({ data }) => {
     const Id = useId();
   return (
-      <div className={style.container}>
+    <div className={style.container}>
+      <Dots className={style.watermark} />
         <div className={style.cards}>
             {data.map((details, i) => (<Card {...details} key={Id + i} />))}
         </div>
