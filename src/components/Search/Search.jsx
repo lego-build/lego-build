af0d46a1965c977cb60f62ec675be47a141ce1dc
Udoka <udochukwukaonyela@gmail.com>
2022-08-26 07:17:33 +0100

    fixes

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -6,7 +6,7 @@ const Search = ({ value: {value, onChange}, resetValue }) => {
     const Id = useId();
   return (
       <div className={style.search}>
-          <label for={Id} className={style.label}>
+          <label htmlFor={Id} className={style.label}>
               <span className={style.icon}><SearchBar/> </span>
               <input value={value} onChange={onChange} type={"text"} id={Id} className={style.input} placeholder="Search for workflow" />
               <span className={`${style.clear} ${value === "" ? style.hidden : style.show}`} onClick={resetValue}><Clear /> </span>
