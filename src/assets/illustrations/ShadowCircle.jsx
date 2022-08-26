34cebb87058850fb0866cf6e32fe65cdd128211f
Udoka <udochukwukaonyela@gmail.com>
2022-08-26 16:51:41 +0100

    added props

--- src/assets/illustrations/ShadowCircle.jsx
+++ src/assets/illustrations/ShadowCircle.jsx
@@ -1,6 +1,6 @@
-const ShadowCircle = () => {
+const ShadowCircle = (props) => {
     return (
-        <svg width="345" height="345" viewBox="0 0 345 345" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg {...props} width="345" height="345" viewBox="0 0 345 345" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g filter="url(#filter0_f_288_1081)">
             <rect x="85" y="85" width="175" height="175" rx="87.5" fill="url(#paint0_linear_288_1081)" fill-opacity="0.09" />
         </g>
