84e6a8c978b179f89958505f6028c17cb444275f
Udoka <udochukwukaonyela@gmail.com>
2022-08-26 11:11:07 +0100

    corrected naming

---
+++ src/assets/illustrations/TransparentRectangle.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const TransparentRectange = (props) => {
+    return (
+        <svg {...props} width="193" height="161" viewBox="0 0 193 161" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="41.7351" y="-140.403" width="213" height="213" transform="rotate(45 41.7351 -140.403)" fill="#FAFCFF" />
+        </svg>
+    );
+}
+ 
+export default TransparentRectange;
