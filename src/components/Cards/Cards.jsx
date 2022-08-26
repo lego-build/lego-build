21715926e44169a3ef7c684e3d46b60b4091c0b4
Udoka <udochukwukaonyela@gmail.com>
2022-08-26 13:42:06 +0100

    styling

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import Card from "../Card/Card"
 import style from "./index.module.css"
 import { useId } from 'react';
-import { Dots } from "../../assets";
+import { Dots, LeftDots } from "../../assets";
 const Cards = ({ data }) => {
     const Id = useId();
   return (
     <div className={style.container}>
       <Dots className={style.watermark} />
+      <LeftDots className={style.leftWatermark} />
         <div className={style.cards}>
             {data.map((details, i) => (<Card {...details} key={Id + i} />))}
-        </div>
+      </div>
       </div>
   )
 }
