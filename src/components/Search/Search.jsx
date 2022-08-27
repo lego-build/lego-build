bbb804d85a859f9abb7d0d0e55ead93e8f9ea539
Akpeti Trust <akpetitrust@gmail.com>
2022-08-27 07:09:34 +0100

    Refactored header

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
-import { useId } from 'react';
+import React from "react";
+import { useId } from "react";
 import { SearchBar, Clear } from "../../assets";
 import style from "./index.module.css";
 const Search = ({ value, resetValue }) => {
-    const Id = useId();
+  const Id = useId();
   return (
-      <div className={style.search}>
-          <label htmlFor={Id} className={style.label}>
-              <span className={style.icon}><SearchBar/> </span>
-              <input value={value} onChange={(e)=>resetValue(e.target.value)} type={"text"} id={Id} className={style.input} placeholder="Search for workflow" />
-              <span className={`${style.clear} ${value === "" ? style.hidden : style.show}`} onClick={()=>resetValue("")}><Clear /> </span>
-          </label>
-          <a href="/#" className={style.contribute}>Contribute Yours</a>
-      </div>
-  )
-}
+    <div className={style.search}>
+      <label htmlFor={Id} className={style.label}>
+        <span className={style.icon}>
+          <SearchBar />{" "}
+        </span>
+        <input
+          value={value}
+          onChange={(e) => resetValue(e.target.value)}
+          type={"text"}
+          id={Id}
+          className={style.input}
+          placeholder="Search for workflow"
+        />
+        <span
+          className={`${style.clear} ${
+            value === "" ? style.hidden : style.show
+          }`}
+          onClick={() => resetValue("")}
+        >
+          <Clear />{" "}
+        </span>
+      </label>
+      <a href="/#" className={style.contribute}>
+        Contribute Yours
+      </a>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
--- src/pages/Community/subpage/Main/Main.jsx
+++ src/pages/Community/subpage/Main/Main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { Cards, Header , Search } from "../../../../components";
+import { Cards, Header, Search } from "../../../../components";
 const Main = ({ value, resetValue, workflows }) => {
-    return (
-        <>
-            <Header />
-            <Search value={value} resetValue={resetValue} />
-            <Cards data={workflows} />
-        </>
-    );
-}
- 
+  return (
+    <>
+      <Header />
+      <Search value={value} resetValue={resetValue} />
+      <Cards data={workflows} />
+    </>
+  );
+};
+
 export default Main;
