4ebea7adc07fea6352d7dd18a18059c6a130b7e6
Udoka <udochukwukaonyela@gmail.com>
2022-08-30 00:07:58 +0100

    exported home page

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Community, Contact, Docs } from "./pages";
+import { Community, Contact, Docs, Home } from "./pages";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
+        <Route path="/" exact element={<Home />} />
         <Route path="/contact" exact element={<Contact />} />
         <Route path="/docs" exact element={<Docs />} />
         <Route path="/docs/:subpage" exact element={<Docs />} />
