450db77acf66337b980d6edff43c4d80dbcbae0c
Udoka <udochukwukaonyela@gmail.com>
2022-09-04 00:04:03 +0100

    added content

--- src/components/Productivity/Productivity.jsx
+++ src/components/Productivity/Productivity.jsx
@@ -1,7 +1,13 @@
+import { ProductivityIllustration } from "../../assets";
+import style from "./index.module.css";
 const Features = () => {
     return (  
-        <section>
-          Features  
+        <section className={style.container}>
+        <ProductivityIllustration className={style.illustration} />
+        <div className={style.content}>
+          <h1 className={style.header}>Improve productivity</h1>
+          <p className={style.details}>Speed up your workflow with lego-build. Create any React block (component, page etc) from your terminal. Don’t waste time writing boilerplate code, let’s do the boring stuff for you, so you just focus on building your wonderful application. <span> Move fast .</span></p>
+        </div>
         </section>
     );
 }
