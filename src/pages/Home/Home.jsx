eb3fb53a1b53014c01acdb9282b36aea2cffa1c8
Udoka <udochukwukaonyela@gmail.com>
2022-09-03 23:36:23 +0100

    renamed component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
-import { Community, Content, CTA, Features, Hero, Layout, Problem, Reason } from "../../components";
+import { Community, Content, CTA, Productivity, Hero, Layout, Problem, Reason } from "../../components";
 
 const Home = () => {
     return (
         <Layout>
             <Hero />
             <Problem />
-            <Features />
+            <Productivity />
             <Content />
             <Reason />
             <Community />
