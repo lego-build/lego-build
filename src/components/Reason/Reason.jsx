6903d93eca3890302f093104a74853f567b4b1ad
Udoka <udochukwukaonyela@gmail.com>
2022-09-04 14:40:31 +0100

    created reason componen

--- src/components/Reason/Reason.jsx
+++ src/components/Reason/Reason.jsx
@@ -8,13 +8,13 @@ const Reason = () => {
                 <h1 className={style.heading}>Why us?</h1>
                 <p className={style.detail}>There are already a few CLI tools available for easily creating components, so why use lego-build? Good question.</p>
             </div>
-            {/* <div className={style.cards}>
+            <div className={style.cards}>
                 <ReasonCard />
                 <ReasonCard />
                 <ReasonCard />
-            </div> */}
+            </div>
             <BlurBlob className={style.watermark} />
-            <p>Convinced that we're Good?</p>
+            <p className={style.cta}>Convinced that we're good? <span className={style.link}>Get Started</span></p>
     </section>
     );
 }
