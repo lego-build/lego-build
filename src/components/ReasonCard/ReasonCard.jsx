4b8fad59f1f9138a52a333e41c40724fd3805d76
Udoka <udochukwukaonyela@gmail.com>
2022-09-04 13:36:14 +0100

    created component for cards in the details section

---
+++ src/components/ReasonCard/ReasonCard.jsx
@@ -0,0 +1,13 @@
+import { ClarityBlockLine } from "../../assets";
+
+const ReasonCard = () => {
+    return ( 
+        <div>
+            <span><ClarityBlockLine /></span>
+            <h3>BLOCK CONCEPT</h3>
+            <p>While other tools just see ‘components’, we see all the parts your React app is composed of as blocks. So you can easily create other special files like hooks, reducers etc. Not just ‘components’.</p>
+        </div>
+     );
+}
+ 
+export default ReasonCard;
