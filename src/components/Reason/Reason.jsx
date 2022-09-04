70be8a1f7842dfee4adf41c8722c964c3575e9ad
Udoka <udochukwukaonyela@gmail.com>
2022-09-04 13:36:34 +0100

    created reason component

--- src/components/Reason/Reason.jsx
+++ src/components/Reason/Reason.jsx
@@ -1,10 +1,18 @@
+import ReasonCard from "../ReasonCard/ReasonCard";
+import style from "./index.module.css";
 const Reason = () => {
     return (
-    <section>
-            <div>
-                <h1></h1>
-        </div>
-            
+    <section className={style.reason}>
+            <div className={style.details}>
+                <h1 className={style.header}>Why us?</h1>
+                <p className={style.details}>There are already a few CLI tools available for easily creating components, so why use lego-build? Good question.</p>
+            </div>
+            <div className={style.cards}>
+                <ReasonCard />
+                <ReasonCard />
+                <ReasonCard />
+            </div>
+            <p>Convinced that we're Good?</p>
     </section>
     );
 }
