c3be5817f810fec20c9c6b09c53928899ff1729f
Udoka <udochukwukaonyela@gmail.com>
2022-09-04 13:55:38 +0100

    added Blur icons

---
+++ src/assets/icons/BlurBlob.jsx
@@ -0,0 +1,22 @@
+const BlurBlob = () => {
+    return ( 
+        <svg width="324" height="464" viewBox="0 0 324 464" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_f_36_1119)">
+                <circle cx="92" cy="232" r="146" fill="url(#paint0_linear_36_1119)" fill-opacity="0.2" />
+            </g>
+            <defs>
+                <filter id="filter0_f_36_1119" x="-139.049" y="0.951454" width="462.097" height="462.097" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                    <feGaussianBlur stdDeviation="42.5243" result="effect1_foregroundBlur_36_1119" />
+                </filter>
+                <linearGradient id="paint0_linear_36_1119" x1="67.9029" y1="53.3981" x2="143.029" y2="328.861" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#9D50F6" />
+                    <stop offset="1" stop-color="#00F5C8" />
+                </linearGradient>
+            </defs>
+        </svg>
+     );
+}
+ 
+export default BlurBlob;
