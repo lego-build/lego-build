562664c357c413e9b4b21ca63aa82c4cd779c3ca
Udoka <udochukwukaonyela@gmail.com>
2022-09-04 13:55:57 +0100

    modified boileerplate

--- src/components/Reason/Reason.jsx
+++ src/components/Reason/Reason.jsx
@@ -2,16 +2,16 @@ import ReasonCard from "../ReasonCard/ReasonCard";
 import style from "./index.module.css";
 const Reason = () => {
     return (
-    <section className={style.reason}>
+    <section className={style.container}>
             <div className={style.details}>
-                <h1 className={style.header}>Why us?</h1>
-                <p className={style.details}>There are already a few CLI tools available for easily creating components, so why use lego-build? Good question.</p>
+                <h1 className={style.heading}>Why us?</h1>
+                <p className={style.detail}>There are already a few CLI tools available for easily creating components, so why use lego-build? Good question.</p>
             </div>
-            <div className={style.cards}>
+            {/* <div className={style.cards}>
                 <ReasonCard />
                 <ReasonCard />
                 <ReasonCard />
-            </div>
+            </div> */}
             <p>Convinced that we're Good?</p>
     </section>
     );
