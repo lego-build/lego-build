ab0c0760a592c8e2b3ac169ac88ca7b416594ff5
Udoka <udochukwukaonyela@gmail.com>
2022-09-04 14:40:46 +0100

    created reason card

--- src/components/ReasonCard/ReasonCard.jsx
+++ src/components/ReasonCard/ReasonCard.jsx
@@ -1,9 +1,9 @@
 import { ClarityBlockLine } from "../../assets";
-
-const ReasonCard = () => {
+import style from "./index.module.css";
+const ReasonCard = ({icons, headings, details}) => {
     return ( 
-        <div>
-            <span><ClarityBlockLine /></span>
+        <div className={style.container}>
+            <span className={style.icon}><ClarityBlockLine /></span>
             <h3>BLOCK CONCEPT</h3>
             <p>While other tools just see ‘components’, we see all the parts your React app is composed of as blocks. So you can easily create other special files like hooks, reducers etc. Not just ‘components’.</p>
         </div>
