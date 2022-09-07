c589905293d7677e79b378ec3552989a281d878d
Udoka <udochukwukaonyela@gmail.com>
2022-09-07 21:21:31 +0100

    created component

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,16 +1,18 @@
+import { ComputerTroubleshootingIllustration } from "../../assets";
 import style from "./index.module.css";
 const Content = () => {
     return (
-        <section>
-            <div>
-                <h1>Power to the developer</h1>  
-                <div>
+        <section className={style.container}>
+            <div className={style.content}>
+                <h1 className={style.header}>Power to the developer</h1>
+                <div className={style.details}>
                     <p>React, by design, is unopinionated. The implication of this is that while every developer tries to maintain some structure in their project, this structure can vary for each developer. Some developers store their all their components in a ‘src/components’ folder, others follow atomic design, so they have folders for atoms, molecules etc. While most React developers have the concept of ‘components’ in their app, there are other building blocks that could be in a React app, there are layouts, pages, reducers, actions etc.</p>
                     <p>So you see, a tool that aims to help developers easily build blocks must be very flexible, and not hard-coded. That’s what <b>lego-build</b> does. All the configuration power lies in the hand of the developer. It’s a <b>flexible</b> tool, that bends to the will of its master.</p>
                 </div>
-           </div>
+            </div>
+            <ComputerTroubleshootingIllustration className={style.illustration} />
         </section>
     );
 }
- 
+
 export default Content;
