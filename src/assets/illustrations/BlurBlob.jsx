c3ad2aac76691e43e387d6a845789795b83ef3d4
Udoka <udochukwukaonyela@gmail.com>
2022-09-07 19:25:33 +0100

    changed floodopacity and stopcor properties to jsx

--- src/assets/illustrations/BlurBlob.jsx
+++ src/assets/illustrations/BlurBlob.jsx
@@ -5,14 +5,14 @@ const BlurBlob = (props) => {
                 <circle cx="92" cy="232" r="146" fill="url(#paint0_linear_36_1119)" fill-opacity="0.2" />
             </g>
             <defs>
-                <filter id="filter0_f_36_1119" x="-139.049" y="0.951454" width="462.097" height="462.097" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <filter id="filter0_f_36_1119" x="-139.049" y="0.951454" width="462.097" height="462.097" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
                     <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
                     <feGaussianBlur stdDeviation="42.5243" result="effect1_foregroundBlur_36_1119" />
                 </filter>
                 <linearGradient id="paint0_linear_36_1119" x1="67.9029" y1="53.3981" x2="143.029" y2="328.861" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#9D50F6" />
-                    <stop offset="1" stop-color="#00F5C8" />
+                    <stop stopColor="#9D50F6" />
+                    <stop offset="1" stopColor="#00F5C8" />
                 </linearGradient>
             </defs>
         </svg>
