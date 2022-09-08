ef05ab9400bcb7b1bfea2514d208cec88ad46651
Udoka <udochukwukaonyela@gmail.com>
2022-09-08 21:12:47 +0100

    added classNames

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -1,13 +1,13 @@
 import style from "./index.module.css";
 const CTA = () => {
     return (
-        <section>
-            <div>
+        <section className={style.container}>
+            <div className={style.content}>
                 <h1>Appreciate the tool?</h1>
                 <p>This tool is currently being maintained and refined by a small team of broke devs working from the trenches, if you appreciate it, abeg epp boys and show support.</p>
                 <span>SUPPORT</span>
             </div>
-            <div>
+            <div className={style.team}>
                 <p>THE O.G. TEAM</p>
                 
             </div>
