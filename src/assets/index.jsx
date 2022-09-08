01e1b544a5d6cf720d9603bae7e8f31c1b5f5c2d
Udoka <udochukwukaonyela@gmail.com>
2022-09-08 20:50:56 +0100

    added bottom left component

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,3 +1,4 @@
+export { default as AppreciateBottomRightWatermark } from "./illustrations/AppreciateBottomRightWatermark";
 export { default as AppreciateTopLeft } from "./illustrations/AppreciateTopLeft";
 export { default as Arrow } from "./illustrations/Arrow";
 export { default as Block } from "./illustrations/Block";
