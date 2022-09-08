cb0d3d15a6148bfbb26646104bffcd8f6640b1fa
Udoka <udochukwukaonyela@gmail.com>
2022-09-08 20:48:16 +0100

    created appreciate bottom left tool

---
+++ src/assets/illustrations/AppreciateBottomRightWatermark.jsx
@@ -0,0 +1,23 @@
+const AppreciateBottomRightWatermark = () => {
+    return ( 
+        <svg width="252" height="275" viewBox="0 0 252 275" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g opacity="0.7">
+                <path opacity="0.7" d="M375.28 141.304L152.122 361.582" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M373.697 123.151L136.512 357.657" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M366.737 110.46L122.832 351.772" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M356.857 100.715L107.495 347.557" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M349.21 88.7144L92.0259 343.486" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M340.179 78.115L75.5969 340.384" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M331.218 67.4397L63.069 333.338" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M322.248 56.7829L47.6911 329.171" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M312.994 46.4146L35.2238 322.057" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M304.842 34.9145L24.8787 312.794" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M295.568 24.5657L13.179 304.902" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M285.718 14.8024L1.65185 296.829" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M277.445 3.43927L3.42347 275.289" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+            </g>
+        </svg>
+     );
+}
+ 
+export default AppreciateBottomRightWatermark;
