6a1c96b070c7afa964a3de77907eb1bf40cfb0d2
Udoka <udochukwukaonyela@gmail.com>
2022-09-08 21:14:12 +0100

    added logic for details

---
+++ src/components/CtaTeam/CtaTeam.jsx
@@ -0,0 +1,10 @@
+const CtaTeam = ({name, details}) => {
+    return ( 
+        <div>
+            <h3>{ name }</h3>
+            <p> { details }</p>
+        </div>
+     );
+}
+ 
+export default CtaTeam;
