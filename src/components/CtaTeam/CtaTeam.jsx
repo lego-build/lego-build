e64ea915b4a3c60decd75ddaa43ce5dbc9ef5670
Udoka <udochukwukaonyela@gmail.com>
2022-09-08 21:35:01 +0100

    created component for team name and roles

--- src/components/CtaTeam/CtaTeam.jsx
+++ src/components/CtaTeam/CtaTeam.jsx
@@ -1,6 +1,7 @@
-const CtaTeam = ({name, details}) => {
+import style from "./index.module.css"
+const CtaTeam = ({ name, details }) => {
     return ( 
-        <div>
+        <div className={style.container}>
             <h3>{ name }</h3>
             <p> { details }</p>
         </div>
