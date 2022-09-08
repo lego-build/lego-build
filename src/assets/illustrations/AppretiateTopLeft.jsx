bf3595186440431bc2e3eaff14e878c1bfc6c533
Udoka <udochukwukaonyela@gmail.com>
2022-09-08 20:44:36 +0100

    created watermark component

---
+++ src/assets/illustrations/AppretiateTopLeft.jsx
@@ -0,0 +1,24 @@
+
+const AppretiateTopLeft = () => {
+    return ( 
+        <svg width="189" height="255" viewBox="0 0 189 255" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g opacity="0.7">
+                <path opacity="0.7" d="M-185 116.733L35.88 -105.583" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M-183.23 134.866L51.5301 -101.806" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M-176.14 147.488L65.2699 -96.0518" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M-166.16 157.136L80.6499 -91.9834" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M-158.39 169.06L96.1598 -88.0605" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M-149.25 179.57L112.62 -85.1157" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M-140.18 190.157L125.22 -78.1899" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M-131.1 200.725L140.64 -74.1699" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M-121.74 211.003L153.18 -67.1763" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M-113.47 222.423L163.62 -58.0127" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M-104.09 232.681L175.4 -50.2344" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M-94.1401 242.348L187.01 -42.272" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+                <path opacity="0.7" d="M-85.75 253.633L185.46 -20.7192" stroke="#DEDEDE" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.3 0.3" />
+            </g>
+        </svg>
+     );
+}
+ 
+export default AppretiateTopLeft;
