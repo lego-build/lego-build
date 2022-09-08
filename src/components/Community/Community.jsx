17fd59e08c095bb1ce48eb87ddef46d959086f2c
Udoka <udochukwukaonyela@gmail.com>
2022-09-08 12:38:26 +0100

    added icon

--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { CommunityPageIllustration } from "../../assets";
+import { CommunityLinkIcon, CommunityPageIllustration } from "../../assets";
 import style from "./index.module.css";
 const Community = () => {
     return (
@@ -7,7 +7,7 @@ const Community = () => {
             <div className={style.content}>
                 <h1 className={style.header}>Get help from a <span>vast</span> community</h1>
                 <p className={style.details}>Finding a front end structure that scales well is hard but fortunately for us, there’s a whole community of people willing to share their scalable workflow with us. See how other developers are using lego-build. Check out their workflows, templates and setups. You’re welcome to share yours with the community too!</p>
-                <Link to={"/community"}><span className={style.communityLink}>View Community</span></Link>
+                <Link to={"/community"}><span className={style.linkContainer}><span className={style.communityLink}>View Community</span> <CommunityLinkIcon className={style.linkIcon} /></span></Link>
             </div>
             <div>
                 <CommunityPageIllustration className={style.illustration} />
