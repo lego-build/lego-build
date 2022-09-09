ed6a5b81768824783d860bab6000aa7516b8fe56
Udoka <udochukwukaonyela@gmail.com>
2022-09-09 09:36:28 +0100

    added watermarks

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -1,4 +1,4 @@
-import { AppreciateTopLeft } from "../../assets";
+import { AppreciateBottomRightWatermark, AppreciateBottomTopRight, AppreciateTopLeft, GearIllustration } from "../../assets";
 import CtaTeam from "../CtaTeam/CtaTeam";
 import style from "./index.module.css";
 const ogTeam = [
@@ -9,7 +9,9 @@ const ogTeam = [
 const CTA = () => {
     return (
         <section className={style.container}>
-            <AppreciateTopLeft className={style.topLeft} /> 
+            <AppreciateTopLeft className={style.topLeft} />
+            <AppreciateBottomRightWatermark className={style.bottomLeft} />
+            <AppreciateBottomTopRight className={style.bottomTopLeft} />
             <div className={style.content}>
                 <h1>Appreciate the tool?</h1>
                 <p>This tool is currently being maintained and refined by a small team of broke devs working from the trenches, if you appreciate it, abeg epp boys and show support.</p>
@@ -21,6 +23,9 @@ const CTA = () => {
                     {ogTeam.map((data, i)=><CtaTeam {...data} key={i} />)}
                 </div>
             </div>
+            <GearIllustration className={style.gear} />
+            <GearIllustration className={style.gear2} />
+            <GearIllustration className={style.gear3} />
         </section>
     );
 }
