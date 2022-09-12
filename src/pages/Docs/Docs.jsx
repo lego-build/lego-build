4e67931bad12bf9cdd0225921556f70a7da0c5e3
Akpeti Trust <akpetitrust@gmail.com>
2022-09-12 18:00:22 +0100

    Fixed scroll issue

--- src/pages/Docs/Docs.jsx
+++ src/pages/Docs/Docs.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import {  SideNav, Layout } from "../../components";
+import { SideNav, Layout } from "../../components";
 import style from "./index.module.css";
 import { Blocks, Formats, Main } from "./subpages";
 
 function Docs() {
   const { subpage } = useParams();
+  const { hash } = useLocation();
 
   let component = <Main />;
   const subpageToComponent = {
@@ -17,13 +19,21 @@ function Docs() {
     component = subpageToComponent[subpage];
   }
 
+  useEffect(() => {
+    if (hash) {
+      const id = hash.replace("#", "");
+      const element = document.getElementById(id);
+      element.scrollIntoView();
+    }
+  });
+
   return (
-      <Layout className={style.docs}>
+    <Layout className={style.docs}>
       <div className={style.main}>
         <SideNav />
         {component}
       </div>
-      </Layout>
+    </Layout>
   );
 }
 
