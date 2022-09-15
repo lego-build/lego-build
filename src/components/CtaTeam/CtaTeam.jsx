c6af7eb4069408ec92944c70e860d6c151ac609f
Udoka <udochukwukaonyela@gmail.com>
2022-09-15 13:40:59 +0100

    added rel attributes

--- src/components/CtaTeam/CtaTeam.jsx
+++ src/components/CtaTeam/CtaTeam.jsx
@@ -2,7 +2,7 @@ import style from "./index.module.css";
 const CtaTeam = ({ name, details, link }) => {
   return (
     <div className={style.container}>
-      <a target={"_blank"} href={link} rel="noreferrer">
+      <a target={"_blank"} href={link} rel="noopener noreferrer">
         {name}
       </a>
       <p> {details}</p>
